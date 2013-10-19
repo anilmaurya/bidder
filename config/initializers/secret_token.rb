6102f5d8dfcf793f0454a26bb2e0a48a759ff31a
Anil Maurya <anilmaurya8dec@gmail.com>
2013-10-19 07:50:22 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRumble::Application.config.secret_token = '9d3582b664387a6045fbe35b7029a28f9c805953e1ce8c06d9cceef96ae7d5a7ba82bed738b84b73156e15b05ff50289b54949cddccca62762cb010e5a278956'
