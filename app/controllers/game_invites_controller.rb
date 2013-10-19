5f6192487d5e1aad3bb4b84fd285f2538f9e9d18
sanjiv <sanjiv@joshsoftware.com>
2013-10-19 21:15:25 +0530

    added the user game invitation controller, js , remaining part is pusher api

--- app/controllers/game_invites_controller.rb
+++ app/controllers/game_invites_controller.rb
@@ -3,16 +3,17 @@ class GameInvitesController < ApplicationController
   #Error code 1000 for some custom error, 1001 unexpected error contact admin
   def create
     if request.xhr?
-      @game_invite = GameInvite.new(params[:game_invite].merge!(sender_user_id: current_user.id))
+      game_invite_params = params[:game_invite] || {} 
+      @game_invite = GameInvite.new(game_invite_params.merge!(sender_user_id: current_user.id))
       if @game_invite.save
         #Pusher['presence-invite_' + @game_invite.id].trigger('game_invite', {from: current_user.id})
         render :json => {valid: true}
       else
-        render :json => {valid: false, error_code: '1000'}
+        render :json => {valid: false, error_code: '1000', message: @game_invite.errors.full_messages.join(' | ')}
       end
     end
   rescue Exception => e
-    render :json => {valid: false, error_code: '1001'}
+    render :json => {valid: false, error_code: '1001', message: e.message}
   end 
       
 end
