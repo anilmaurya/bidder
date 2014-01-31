b26c972838a762897e8ae935058657221174d220
Sanjiv Jha <narutosanjiv@gmail.com>
2014-01-31 19:31:59 +0530

    added different api for pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,4 +1,10 @@
-Pusher.app_id = '57169'
-Pusher.key = '0549d325d1840b3a4c05'
-Pusher.secret = '695b40954fec56afd574'    
+if Rails.env.development?
+  Pusher.app_id = '24310'
+  Pusher.key = 'e221bc3495c05b7df9dd'
+  Pusher.secret = 'f4faf47f16352976d518' 
+else
+  Pusher.app_id = '57169'
+  Pusher.key = '0549d325d1840b3a4c05'
+  Pusher.secret = '695b40954fec56afd574'   
+end 
 
