eff49c368c512d7a70c8537c14192a79175e47c7
anilmaurya <anilmaurya8dec@gmail.com>
2014-03-03 23:22:35 +0530

    changing bidder logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,22 +8,23 @@ class ApplicationController < ActionController::Base
   end
   
   protected
-    def get_invitation_request
-      if params[:action] == "create" and params[:controller] == 'devise/sessions'
-        user = User.find_or_initialize_by(username: params[:user][:username])
-        user.password = 'bidder'
-        user.password_confirmation = 'bidder'
-        user.save(validate: false)
-        params[:user].merge!(:password => 'bidder')  
-      end
-      if current_user
-        @invitation_request = GameInvite.where(receiver_user_id: current_user.id, accept_status: nil).first  
+
+  def get_invitation_request
+    if params[:action] == "create" and params[:controller] == 'devise/sessions'
+      user = User.find_or_initialize_by(username: params[:user][:username])
+      user.password = 'bidder'
+      user.password_confirmation = 'bidder'
+      user.save(validate: false)
+      params[:user].merge!(:password => 'bidder')  
+    end
+    if current_user
+      @invitation_request = GameInvite.where(receiver_user_id: current_user.id, accept_status: nil).first  
+    else
+      if params[:user]
+        @user = User.new(params[:user])     
       else
-        if params[:user]
-          @user = User.new(params[:user])     
-        else
-          @user = User.new 
-        end
+        @user = User.new 
       end
     end
+  end
 end
