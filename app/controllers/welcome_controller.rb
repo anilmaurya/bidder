d77e621ab013c75296c19af3e007efe597618ade
adityashedge <aditya@joshsoftware.com>
2014-03-06 14:33:37 +0530

    modified about us page
    -shows list of contributors
    -links contributor name to github account

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-    
+  def index    
     if current_user
       redirect_to :games
     else
       @user = User.new
-    end
-    
+    end    
+  end
+
+  def about_us
+    @contributors = Github.new.repos.contributors "anilmaurya", "bidder"
   end
 end
